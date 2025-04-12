46c843fc64ce5e464db67961b161c961e85bf49c
M Adnan Khan <160040993+Adnan00Khan@users.noreply.github.com>
2025-04-12 19:27:22 +0500

    add

--- Q3-Randomqoute-generator-main/app.py
+++ Q3-Randomqoute-generator-main/app.py
@@ -9,7 +9,7 @@ def get_random_quote():
     st.write(f"Current directory: {current_dir}")  # Display the current directory on the app
 
     try:
-        with open("quotes.txt", "r", encoding="utf-8") as file:
+        with open("q 3\Q3-Randomqoute-generator-main\quotes.txt", "r", encoding="utf-8") as file:
             quotes = file.readlines()
         return random.choice(quotes).strip()
     except FileNotFoundError:
